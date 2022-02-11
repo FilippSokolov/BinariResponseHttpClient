e7492c77982905a9843cf12f0d9be3b99e18d915
Filipp.Sokolov <Filipp.Sokolov@jetbrains.com>
2022-02-11 12:33:44 +0300

    zzz

--- src/main/java/com/example/demo414/HelloResource.java
+++ src/main/java/com/example/demo414/HelloResource.java
@@ -6,11 +6,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
+import java.net.URI;
 
 @Path("/hello-world")
 public class HelloResource {
     @GET
     @Produces("text/plain")
+   // @Produces(MediaType.SERVER_SENT_EVENTS)
     public String hello() {
         return "Hello, World!";
     }
@@ -20,6 +22,57 @@ public class HelloResource {
     private static final String PDFFILE_PATH = "gaenselieselstrasse_projektinfo.pdf";
     private static final String EXCELFILE_PATH = "EXCEL_Example.XLSX";
 
+
+    @GET
+    @Path("/redirect") //   /ExampleResource/text
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getRedirect() {
+
+        Response.ResponseBuilder response = Response.temporaryRedirect(URI.create("http://localhost:8080/demo414_war/api/hello-world/redirect2"));
+        Response.ResponseBuilder response2 = Response.temporaryRedirect(URI.create("https://google.com/"));
+//
+//        Response.temporaryRedirect(URI)
+//
+//        InputStream in = this.getClass().getClassLoader().getResourceAsStream(TEXTFILE_PATH);
+//        Response.ResponseBuilder response = Response.ok(in, MediaType.APPLICATION_OCTET_STREAM);
+//        response.header("Content-Disposition", "attachment; filename=\"" + TEXTFILE_PATH + "\"");
+        return response.build();
+
+    }
+
+ @GET
+    @Path("/redirect2") //   /ExampleResource/text
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getRedirect2() {
+
+        Response.ResponseBuilder response = Response.temporaryRedirect(URI.create("http://localhost:8080/demo414_war/api/hello-world/redirect3"));
+     // Response.ResponseBuilder response2 = Response.temporaryRedirect(URI.create("https://google.com/"));
+//
+//        Response.temporaryRedirect(URI)
+//
+//        InputStream in = this.getClass().getClassLoader().getResourceAsStream(TEXTFILE_PATH);
+//        Response.ResponseBuilder response = Response.ok(in, MediaType.APPLICATION_OCTET_STREAM);
+//        response.header("Content-Disposition", "attachment; filename=\"" + TEXTFILE_PATH + "\"");
+        return response.build();
+
+    }
+
+    @GET
+    @Path("/redirect3") //   /ExampleResource/text
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getRedirect3() {
+
+        Response.ResponseBuilder response = Response.temporaryRedirect(URI.create("http://localhost:8080/demo414_war/api/hello-world/text"));
+       // Response.ResponseBuilder response2 = Response.temporaryRedirect(URI.create("https://google.com/"));
+//
+//        Response.temporaryRedirect(URI)
+//
+//        InputStream in = this.getClass().getClassLoader().getResourceAsStream(TEXTFILE_PATH);
+//        Response.ResponseBuilder response = Response.ok(in, MediaType.APPLICATION_OCTET_STREAM);
+//        response.header("Content-Disposition", "attachment; filename=\"" + TEXTFILE_PATH + "\"");
+        return response.build();
+
+    }
 
     @GET
     @Path("/text") //   /ExampleResource/text
